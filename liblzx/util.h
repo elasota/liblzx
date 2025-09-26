e4347550322a638629c7fbb6f4e7013638e50a84
elasota <1137273+elasota@users.noreply.github.com>
2025-09-26 08:49:23 -0400

    Remove unused functions

--- liblzx/util.h
+++ liblzx/util.h
@@ -11,84 +11,10 @@
  * General macros
  *****************/
 
-/* Cast a pointer to a struct member to a pointer to the containing struct.  */
-#define container_of(ptr, type, member) \
-        ((type *)((char *)(ptr) - offsetof(type, member)))
-
 /* Calculate 'n / d', but round up instead of down.  */
 #define DIV_ROUND_UP(n, d)        (((n) + (d) - 1) / (d))
 
-/* Calculate 'n % d', but return 'd' if the result would be 0.  */
-#define MODULO_NONZERO(n, d)        (((n) % (d)) ? ((n) % (d)) : (d))
-
 /* Get the number of elements of an array type.  */
 #define ARRAY_LEN(array)        (sizeof(array) / sizeof((array)[0]))
-
-/* Round 'v' up to the next 'alignment'-byte aligned boundary.  'alignment' must
- * be a power of 2.  */
-#undef ALIGN        /* NetBSD <sys/param.h> defines this already */
-#define ALIGN(v, alignment)        (((v) + ((alignment) - 1)) & ~((alignment) - 1))
-
-/* Maximum number of bytes that can be allocated on the stack.
- *
- * Note: this isn't a hard bound on the stack space used, since this is just for
- * individual arrays.  The full call stack could use more than this.  */
-#define STACK_MAX 32768
-
-/* Default size of file I/O buffer.  Currently assumed to be <= STACK_MAX.  */
-#define BUFFER_SIZE 32768
-
-/*******************
- * String utilities
- *******************/
-
-#ifndef HAVE_MEMPCPY
-void *
-mempcpy(void *dst, const void *src, size_t n);
-#endif
-
-/**************************
- * Random number generation
- **************************/
-
-void
-get_random_bytes(void *p, size_t n);
-
-/************************
- * Hashing and comparison
- ************************/
-
-static inline bool
-is_power_of_2(unsigned long n)
-{
-        return (n != 0 && (n & (n - 1)) == 0);
-
-}
-
-static inline uint64_t
-hash_u64(uint64_t n)
-{
-        return n * 0x9e37fffffffc0001ULL;
-}
-
-static inline int
-cmp_u32(uint32_t n1, uint32_t n2)
-{
-        if (n1 < n2)
-                return -1;
-        if (n1 > n2)
-                return 1;
-        return 0;
-}
-
-static inline int
-cmp_u64(uint64_t n1, uint64_t n2)
-{
-        if (n1 < n2)
-                return -1;
-        if (n1 > n2)
-                return 1;
-        return 0;
-}
 
 #endif /* _LIBLZX_UTIL_H */
